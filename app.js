5f50e5b5bd48ac3d0382c604fa4ef600c664becd
prraneshvm <prranesh.vm@gmail.com>
2024-06-19 14:58:50 +0530

    fix

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const Company = require('./model/company');
 
 // CORS options
 const corsOptions = {
-  origin: 'http://localhost:3001', // The client origin
+  origin: 'http://192.168.101.74/', // The client origin
   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 };
 
