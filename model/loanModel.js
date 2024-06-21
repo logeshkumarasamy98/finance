0092ce968a096d7ecff68f319f5b3a1800d936f2
logeshkumarasamy98 <logeshlog420@gmail.com>
2024-06-21 21:34:49 +0530

    Changes to be committed:

--- model/loanModel.js
+++ model/loanModel.js
@@ -92,6 +92,71 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+// userSchema.pre('save', async function(next) {
+//     try {
+//         if (this.isNew && this.loanDetails.instalment && this.loanDetails.instalmentObject.length === 0) {
+//             const { instalment, startDate, totalPrincipalAmount, interestRate } = this.loanDetails;
+//             const monthlyInterestRate = interestRate / 100; // Convert annual interest rate to monthly rate
+
+//             let monthlyPrincipal = totalPrincipalAmount / instalment;
+//             let totalLoan = totalPrincipalAmount;
+
+//             for (let i = 1; i <= instalment; i++) {
+//                 let currentDate = new Date(startDate); // Create a new Date object for each iteration
+//                 currentDate.setMonth(currentDate.getMonth() + i); // Increment month by i
+                
+//                 const monthlyInterestAmount = totalLoan * monthlyInterestRate;
+//                 const monthlyTotalAmount = monthlyInterestAmount + monthlyPrincipal;
+
+//                 // Custom rounding function to round off to the next 5
+//                 const roundToNext5 = (num) => Math.ceil(num / 5) * 5;
+
+//                 // Round off monthlyPrincipal and monthlyInterestAmount to the next 5
+//                 monthlyPrincipal = roundToNext5(monthlyPrincipal);
+//                 const roundedMonthlyInterestAmount = roundToNext5(monthlyInterestAmount);
+
+//                 // const monthlyTotalAmount = monthlyPrincipal + roundedMonthlyInterestAmount
+
+//                 const roundedTotalEmiAmount = (monthlyPrincipal + roundedMonthlyInterestAmount); // Round off to nearest 10-digit number
+
+//                 this.loanDetails.instalmentObject.push({
+//                     installmentNo: i,
+//                     isPaid: false,
+//                     dueDate: currentDate,
+//                     paidDate: null,
+//                     emiPaid: null,
+//                     receiptNumber: null,
+//                     principleAmountPerMonth: monthlyPrincipal,
+//                     interestAmount: roundedMonthlyInterestAmount,
+//                     totalEmiAmount: monthlyTotalAmount,
+//                     totalEmiAmountRoundoff: roundedTotalEmiAmount,
+//                     overdueAmount: null,
+//                     overduePaid: null,
+//                     overDueBalance: null,
+//                     updatedBy: null // Set the user who created the loan as the updater of the initial installments
+//                 });
+//             }
+
+//             // Calculate totalEmiAmount after all installment objects have been added
+//             const totalEmiAmount = this.loanDetails.instalmentObject.reduce((total, installment) => {
+//                 return total + installment.totalEmiAmount;
+//             }, 0);
+//             const roundedTotalEmiBalance = Math.round(totalEmiAmount);
+
+//             // Set totalEmiAmount in the loanDetails
+//             this.loanDetails.totalEmiAlreadyPaid = 0;
+//             this.loanDetails.totalEmiAmount = totalEmiAmount;
+//             this.loanDetails.totalOverdueAmountToBePaid = null;
+//             this.loanDetails.totalEmiAndOverdueToBePaid = null;
+//             this.loanDetails.totalEmiBalance = roundedTotalEmiBalance;
+//         }
+//         next(); // Call next to proceed with the save operation
+//     } catch (error) {
+//         next(error); // Pass any errors to the error handling middleware
+//     }
+// });
+
+
 userSchema.pre('save', async function(next) {
     try {
         if (this.isNew && this.loanDetails.instalment && this.loanDetails.instalmentObject.length === 0) {
@@ -108,16 +173,11 @@ userSchema.pre('save', async function(next) {
                 const monthlyInterestAmount = totalLoan * monthlyInterestRate;
                 const monthlyTotalAmount = monthlyInterestAmount + monthlyPrincipal;
 
-                // Custom rounding function to round off to the next 5
-                const roundToNext5 = (num) => Math.ceil(num / 5) * 5;
+                // Round off monthlyPrincipal and monthlyInterestAmount to the nearest integer
+                const roundedMonthlyPrincipal = Math.round(monthlyPrincipal);
+                const roundedMonthlyInterestAmount = Math.round(monthlyInterestAmount);
 
-                // Round off monthlyPrincipal and monthlyInterestAmount to the next 5
-                monthlyPrincipal = roundToNext5(monthlyPrincipal);
-                const roundedMonthlyInterestAmount = roundToNext5(monthlyInterestAmount);
-
-                // const monthlyTotalAmount = monthlyPrincipal + roundedMonthlyInterestAmount
-
-                const roundedTotalEmiAmount = (monthlyPrincipal + roundedMonthlyInterestAmount); // Round off to nearest 10-digit number
+                const roundedTotalEmiAmount = Math.round(roundedMonthlyPrincipal + roundedMonthlyInterestAmount);
 
                 this.loanDetails.instalmentObject.push({
                     installmentNo: i,
@@ -126,15 +186,17 @@ userSchema.pre('save', async function(next) {
                     paidDate: null,
                     emiPaid: null,
                     receiptNumber: null,
-                    principleAmountPerMonth: monthlyPrincipal,
+                    principleAmountPerMonth: roundedMonthlyPrincipal,
                     interestAmount: roundedMonthlyInterestAmount,
-                    totalEmiAmount: monthlyTotalAmount,
+                    totalEmiAmount: Math.round(monthlyTotalAmount),
                     totalEmiAmountRoundoff: roundedTotalEmiAmount,
                     overdueAmount: null,
                     overduePaid: null,
                     overDueBalance: null,
                     updatedBy: null // Set the user who created the loan as the updater of the initial installments
                 });
+
+                totalLoan -= monthlyPrincipal; // Deduct the principal from the total loan for next month's calculation
             }
 
             // Calculate totalEmiAmount after all installment objects have been added
